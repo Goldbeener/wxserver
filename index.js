d335955a99da09ed7ef46b002f7e8d2f6d088b22
Goldbeener <duxin@daojia.com>
2018-06-19 19:31:51 +0800

    feat: post

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const koa = require('koa')
 const router = require('koa-router')()
 const crypto = require('crypto')
 const bodyParser = require('koa-bodyparser')
+const xmlParser = require('koa-xml-body')
 
 function sha1(str) {
   let md5sum = crypto.createHash('sha1')
@@ -12,35 +13,34 @@ function sha1(str) {
 
 const app = new koa()
 
-router.get('/wx', async (ctx, next) => {
-  console.log('ctx query---->', ctx.query)
-  let param = ctx.query
-  let {signature, timestamp, nonce, echostr } = param
-  let token = 'liduheart'
+router.all('/wx', async (ctx, next) => {
+  if(ctx.request.method == 'GET'){
+    console.log('ctx query---->', ctx.query)
+    let param = ctx.query
+    let {signature, timestamp, nonce, echostr } = param
+    let token = 'liduheart'
 
-  let list = [token, timestamp, nonce]
-  list.sort()
+    let list = [token, timestamp, nonce]
+    list.sort()
 
-  const origin = list.join('')
+    const origin = list.join('')
 
-  console.log('Origin str----->', origin)
-  console.log('Signature str----->', signature)
+    console.log('Origin str----->', origin)
+    console.log('Signature str----->', signature)
 
-  var scyptoString = sha1(origin)
+    var scyptoString = sha1(origin)
 
-  if(scyptoString == signature){
-    ctx.response.body = echostr
+    if(scyptoString == signature){
+      ctx.response.body = echostr
+    }else{
+      ctx.response.body = 'valid false'
+    }
   }else{
-    ctx.response.body = 'valid false'
+    console.log('获取到的数据------>', ctx.request)
   }
 })
 
-router.post('/wx', async (ctx, next) => {
-  console.log('获取到的数据------>', ctx.request)
-  next()
-})
-
-app.use(bodyParser())
+app.use(xmlParser())
 app.use(router.routes())
 
 app.listen(80)
