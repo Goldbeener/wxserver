1a43d5376ccae67ceea5e773fb0e71cf9366870e
Goldbeener <duxin@daojia.com>
2018-06-19 21:00:01 +0800

    fix: post data

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
-const koa = require('koa')
-const router = require('koa-router')()
-const bodyParser = require('koa-bodyparser')
-const xmlParser = require('koa-xml-body')
+const koa = require("koa");
+const router = require("koa-router")();
+const bodyParser = require("koa-bodyparser");
+const xmlParser = require("koa-xml-body");
 
-const vertufy = require('./modules/vertifyServer.js')
+const vertufy = require("./modules/vertifyServer.js");
 
-const app = new koa()
+const app = new koa();
 
-router.all('/wx', async (ctx, next) => {
-  if(ctx.request.method == 'GET'){
-    vertufy(ctx, next)
-  }else{
-    console.log('获取到的数据------>', ctx.request.body)
-    ctx.body = 'success'
+router.all("/wx", async (ctx, next) => {
+  if (ctx.request.method == "GET") {
+    vertufy(ctx, next);
+  } else {
+    let xmlData = ctx.request.body.xml;
+    console.log("获取到的数据------>", JSON.stringify(xmlData, null, 2));
+
+
+    let me = xmlData.ToUserName[0];
+    let fromUser = xmlData.FromUserName[0];
+    let CreateTime = xmlData.CreateTime[0];
+    let MsgType = xmlData.MsgType[0];
+    let Content = xmlData.content[0];
+    let MsgId = xmlData.MsgId[0] + 1;
+
+    let sendTime = + new Date()
+    let response = '你说的是' + Content + '吗?'
+    let xml = `
+    <xml>  
+      <ToUserName>< ![CDATA[${fromUser}]]></ToUserName>  
+      <FromUserName>< ![CDATA[${me}]]></FromUserName>  
+      <CreateTime>${sendTime}</CreateTime>  
+      <MsgType>< ![CDATA[text]]></MsgType>  
+      <Content>< ![CDATA[${response}]]></Content>  
+      <MsgId>${MsgId}</MsgId>
+    </xml>
+    `;
+
+    ctx.body = xml;
   }
-})
+});
 
-app
-  .use(xmlParser())
-  .use(router.routes())
+app.use(xmlParser()).use(router.routes());
 
-app.listen(80)
-console.log('server is listening 80')
+app.listen(80);
+console.log("server is listening 80");
+
+// {
+//   xml:{
+//     ToUserName: [ 'gh_cb030bbcf306' ],
+//     FromUserName: [ 'oYUKQ0RPQrlEgFRkEDWXbz7fepXs' ],
+//     CreateTime: [ '1529412043' ],
+//     MsgType: [ 'text' ],
+//     Content: [ '后' ],
+//     MsgId: [ '6568774707266261985' ]
+//   }
+// }
