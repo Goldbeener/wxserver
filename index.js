65a2c78568ddcdc68abe6e9c0dabc31291ad7234
Goldbeener <duxin@daojia.com>
2018-06-20 10:17:04 +0800

    fix: post data

--- index.js
+++ index.js
@@ -10,32 +10,32 @@ const app = new koa();
 router.all("/wx", async (ctx, next) => {
   if (ctx.request.method == "GET") {
     vertufy(ctx, next);
-  } else {
-    let xmlData = ctx.request.body.xml;
-    console.log("获取到的数据------>", JSON.stringify(xmlData, null, 2));
+  } else if (ctx.request.method == "POST") {
+    // let xmlData = ctx.request.body.xml;
+    // console.log("获取到的数据------>", JSON.stringify(xmlData, null, 2));
 
 
-    let me = xmlData.ToUserName[0];
-    let fromUser = xmlData.FromUserName[0];
-    let CreateTime = xmlData.CreateTime[0];
-    let MsgType = xmlData.MsgType[0];
-    let Content = xmlData.Content[0];
-    let MsgId = xmlData.MsgId[0] - 1;
+    // let me = xmlData.ToUserName[0];
+    // let fromUser = xmlData.FromUserName[0];
+    // let CreateTime = xmlData.CreateTime[0];
+    // let MsgType = xmlData.MsgType[0];
+    // let Content = xmlData.Content[0];
+    // let MsgId = xmlData.MsgId[0] - 1;
 
-    let sendTime = new Date().getTime()
-    let response = '你说的是' + Content + '吗?'
-    let xml = `
-      <xml>  
-        <ToUserName>< ![CDATA[${fromUser}]]></ToUserName>  
-        <FromUserName>< ![CDATA[${me}]]></FromUserName>  
-        <CreateTime>${sendTime}</CreateTime>  
-        <MsgType>< ![CDATA[text]]></MsgType>  
-        <Content>< ![CDATA[${response}]]></Content>  
-        <MsgId>${MsgId}</MsgId>
-      </xml>
-      `
-    ctx.response.type = 'application/xml'
-    ctx.body = xml;
+    // let sendTime = new Date().getTime()
+    // let response = '你说的是' + Content + '吗?'
+    // let xml = `
+    //   <xml>  
+    //     <ToUserName>< ![CDATA[${fromUser}]]></ToUserName>  
+    //     <FromUserName>< ![CDATA[${me}]]></FromUserName>  
+    //     <CreateTime>${sendTime}</CreateTime>  
+    //     <MsgType>< ![CDATA[text]]></MsgType>  
+    //     <Content>< ![CDATA[${response}]]></Content>  
+    //     <MsgId>${MsgId}</MsgId>
+    //   </xml>
+    //   `
+    // ctx.response.type = 'application/xml'
+    // ctx.body = xml;
   }
 });
 
