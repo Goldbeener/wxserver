d21e0be71a63f23ffe8c6b1b19e2ca5b688e8856
Goldbeener <duxin@daojia.com>
2018-06-20 11:01:44 +0800

    fix: post data

--- modules/replyImg.js
+++ modules/replyImg.js
@@ -1,24 +1,21 @@
 module.exports = (ctx, xmlData) => {
-  let me = xmlData.ToUserName[0];
   let fromUser = xmlData.FromUserName[0];
-  let CreateTime = xmlData.CreateTime[0];
-  let MsgId = xmlData.MsgId[0];
+  let me = xmlData.ToUserName[0];
+  let sendTime = new Date().getTime()
   let PicUrl = xmlData.PicUrl[0];
   let MediaId = xmlData.MediaId[0];
+  let MsgId = xmlData.MsgId[0];
 
-  let sendTime = new Date().getTime()
   let xml = `
     <xml>  
       <ToUserName><![CDATA[${fromUser}]]></ToUserName>  
       <FromUserName><![CDATA[${me}]]></FromUserName>  
       <CreateTime>${sendTime}</CreateTime>  
       <MsgType><![CDATA[image]]></MsgType>
-      <PicUrl><![CDATA[${PicUrl}]]></PicUrl>
-      <MediaId><![CDATA[${MediaId}]]></MediaId>
-      <MsgId>${MsgId}</MsgId>
+      <Image>
+        <MediaId><![CDATA[${MediaId}]]></MediaId>
+      </Image>
     </xml>
-    `
-
-    
+    `    
   ctx.body = xml
 }
