c55f2c1e7a86d1f6a9ef4627dc9c04396a06d7a8
Goldbeener <duxin@daojia.com>
2018-06-20 15:47:40 +0800

    feat: get access_token

--- modules/createMenu.js
+++ modules/createMenu.js
@@ -1,10 +1,12 @@
 const axios = require('axios')
 const url = 'https://api.weixin.qq.com/cgi-bin/menu/create'
+const menu = require('../config').menu
 
 module.exports = (token) => {
   axios.post(url, {
     params: {
-      access_token: token
+      access_token: token,
+      data: menu
     }
   })
 }
