b68be44047fb458db6e259be853fc5207a776aaf
Goldbeener <duxin@daojia.com>
2018-06-20 10:28:13 +0800

    fix: post data

---
+++ modules/replyText.js
@@ -0,0 +1,26 @@
+
+module.exports = (ctx, next) => {
+  let xmlData = ctx.request.body.xml;
+  console.log("获取到的数据------>", JSON.stringify(xmlData, null, 2));
+
+  let me = xmlData.ToUserName[0];
+  let fromUser = xmlData.FromUserName[0];
+  let CreateTime = xmlData.CreateTime[0];
+  let MsgType = xmlData.MsgType[0];
+  let Content = xmlData.Content[0];
+  let MsgId = xmlData.MsgId[0] - 1;
+
+  let sendTime = new Date().getTime()
+  let response = '你说的是' + Content + '吗?'
+  let xml = `
+    <xml>  
+      <ToUserName><![CDATA[${fromUser}]]></ToUserName>  
+      <FromUserName><![CDATA[${me}]]></FromUserName>  
+      <CreateTime>${sendTime}</CreateTime>  
+      <MsgType><![CDATA[text]]></MsgType>  
+      <Content><![CDATA[${response}]]></Content>  
+      <MsgId>${MsgId}</MsgId>
+    </xml>
+    `
+  ctx.body = xml
+}
